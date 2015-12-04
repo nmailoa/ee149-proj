1a31b8f2d69bcb854610b4799f4ca7b596bb8b1b
nmailoa <nmailoa@berkeley.edu>
2015-12-04 13:19:24 -0800

    a bunch of new data

--- accel_to_pos_code/draw.py
+++ accel_to_pos_code/draw.py
@@ -2,13 +2,20 @@ import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 import math
+import sys
 
 from numpy import genfromtxt
 
-data = genfromtxt('v.csv', delimiter=',')
-ax = np.round((data[:,1]-data[0,1])*5)/5
-ay = np.round((data[:,2]-data[0,2])*5)/5
-az = -np.round((data[:,3]-data[0,3])*5)/5
+if (len(sys.argv) != 2):
+  print("Usage: python draw.py filename.csv")
+
+data = genfromtxt(sys.argv[1], delimiter=',')
+#ax = np.round((data[:,1]-data[0,1])*5)/5
+#ay = np.round((data[:,2]-data[0,2])*5)/5
+#az = -np.round((data[:,3]-data[0,3])*5)/5
+ax = (data[:,1]-data[0,1])
+ay = (data[:,2]-data[0,2])
+az = (data[:,3]-data[0,3])
 ez = -(data[:,4]-data[0,4])/360*2*math.pi
 ey = (data[:,5]-data[0,5])/360*2*math.pi
 ex = (data[:,6]-data[0,6])/360*2*math.pi
@@ -23,13 +30,18 @@ cex = np.cos(ex)
 cey = np.cos(ey)
 cez = np.cos(ez)
 
-ori_ax
+ori_ax = np.copy(ax)
+ori_ay = np.copy(ay)
+ori_az = np.copy(az)
 
+# IIR low pass
+alpha = .8
 for i in range(len(ax)-1):
-  ax[i+1] = ax[i]*0.8 + ax[i+1]*0.2
-  ay[i+1] = ay[i]*0.8 + ay[i+1]*0.2
-  az[i+1] = az[i]*0.8 + az[i+1]*0.2
+  ax[i+1] = ax[i]*alpha + ax[i+1]*(1-alpha)
+  ay[i+1] = ay[i]*alpha + ay[i+1]*(1-alpha)
+  az[i+1] = az[i]*alpha + az[i+1]*(1-alpha)
 
+# Move to real coordinates
 absolute_ax = cey*cez*ax + (sex*sey*cez - cex*sez)*ay + (cex*sey*cez + sex*sez)*az
 absolute_ay = cey*sez*ax + (sex*sey*sez + cex*sez)*ay + (cex*sey*sez - sex*cez)*az
 absolute_az = -sey*ax + sex*cey*ay + cex*cey*az
@@ -43,18 +55,20 @@ x = np.zeros(len(ax))
 y = np.zeros(len(ax))
 z = np.zeros(len(ax))
 
-anchor = np.zeros(len(ax))
-anchor[0] = 1
+#anchor = np.zeros(len(ax))
+#anchor[0] = 1
 
+"""
 fig = plt.figure()
 figA = fig.add_subplot(111, projection = "3d")
 figA.scatter(x[0],y[0],z[0])
 #figA.view_init(elev=20, azim=47)
+"""
 
 last_zero = time[0];
 
 for i in range(len(ax)-1):
-  t = (time[i+1] - time[i])
+  t = (time[i+1] - time[i])/1000
 
 
   # update velocity
@@ -66,6 +80,7 @@ for i in range(len(ax)-1):
   y[i+1] = y[i] + vy[i]*t
   z[i+1] = z[i] + vz[i]*t
 
+  """
   if (vx[i+1] <= 0.1 and vy[i+1] <= 0.1 and vz[i+1] <= 0.1):
     if last_zero == 0:
       last_zero = time[i]
@@ -74,7 +89,6 @@ for i in range(len(ax)-1):
   else:
     last_zero = 0    
 
-  """
   # calibrate vel if more than half a second w no accel
   if ((time[i+1] - last_zero) > .5):
     last_anchor = [i for i, e in enumerate(anchor) if e != 0]
@@ -99,12 +113,13 @@ for i in range(len(ax)-1):
   #figA.scatter(x[i+1],y[i+1],z[i+1])
   #figA.plot([x[i],x[i+1]],[y[i],y[i+1]],[z[i],z[i+1]])
   """
-
+"""
 figA.plot(x,y,z)
 figA.set_xlabel('x')
 figA.set_ylabel('y')
 figA.set_zlabel('z')
 plt.show()
+"""
 
 fig = plt.figure()
 #plt.plot(x)
@@ -113,29 +128,43 @@ fig = plt.figure()
 #plt.plot(vx)
 #plt.plot(vy)
 #plt.plot(vz)
-figA = fig.add_subplot(311)
-figA.plot(ax)
-figA.plot(ay)
-figA.plot(az)
-plt.legend(['ax', 'ay', 'az'],loc='center left', bbox_to_anchor=(1, 0.5))
+figA = fig.add_subplot(511)
+figA.plot(ori_ax)
+figA.plot(ori_ay)
+figA.plot(ori_az)
+#plt.xlim([0,100])
+plt.title('raw accel')
+plt.legend(['x', 'y', 'z'],loc='center left', bbox_to_anchor=(1, 0.5))
 
-figB = fig.add_subplot(312)
+figB = fig.add_subplot(512)
+figB.plot(ax)
+figB.plot(ay)
+figB.plot(az)
+#plt.xlim([0,100])
+plt.title('filtered accel')
+plt.legend(['x', 'y', 'z'],loc='center left', bbox_to_anchor=(1, 0.5))
+
+figB = fig.add_subplot(513)
 figB.plot(absolute_ax)
 figB.plot(absolute_ay)
 figB.plot(absolute_az)
-plt.legend(['absolute_ax', 'absolute_ay', 'absolute_az'],loc='center left', bbox_to_anchor=(1, 0.5))
+#plt.xlim([0,100])
+plt.title('absolute accel')
+plt.legend(['x', 'y', 'z'],loc='center left', bbox_to_anchor=(1, 0.5))
 
-
-figC = fig.add_subplot(313)
+figC = fig.add_subplot(514)
 figC.plot(vx)
 figC.plot(vy)
 figC.plot(vz)
-plt.legend(['vx', 'vy', 'vz'],loc='center left', bbox_to_anchor=(1, 0.5))
+plt.title('vel')
+plt.legend(['x', 'y', 'z'],loc='center left', bbox_to_anchor=(1, 0.5))
 
-fig = plt.figure()
-figD = fig.add_subplot(111)
+figD = fig.add_subplot(515)
 figD.plot(x)
 figD.plot(y)
 figD.plot(z)
+plt.title('position')
 plt.legend(['x', 'y', 'z'],loc='center left', bbox_to_anchor=(1, 0.5))
+
+plt.tight_layout()
 plt.show()
